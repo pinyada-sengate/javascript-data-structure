151fb964b9c92e51866d4c72dda0d62bae5dc3d5
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-06-27 22:49:44 +0200

    queue dequeue

--- queue.js
+++ queue.js
@@ -1,15 +1,15 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
   constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
-  }
-}
-
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
   }
 
   enqueue(value) {
@@ -25,4 +25,30 @@ class Node {
 
     return ++this.size;
   }
+
+  dequeue() {
+    if (!this.first && !this.last) {
+      return null;
+    }
+
+    var dequeueNode = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+
+    this.size--;
+    return dequeueNode.value;
+  }
 }
+
+var queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
