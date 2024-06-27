0f730adbbbddafc5be2d9afcd10a2d68a7a5a504
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-06-27 17:24:58 +0200

    queue

---
+++ queue.js
@@ -0,0 +1,14 @@
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
