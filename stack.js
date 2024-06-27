b3ad67dd9157d61bda8e1c81f0668da61871fed7
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-06-27 14:00:56 +0200

    stack push

---
+++ stack.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    var newNode = new Node(value);
+
+    if (!this.first && !this.last) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+}
