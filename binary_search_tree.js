2cbe921ff0660fa9b19ce8c5a67b08839d904c1f
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-06-28 16:24:13 +0200

    binary search tree insert

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -10,4 +10,38 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //go to the left
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (value > currentNode.value) {
+          //go to the right
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        } else {
+          return this;
+        }
+      }
+    }
+  }
 }
+
+var tree = new BinarySearchTree();
+tree.insert(10);
