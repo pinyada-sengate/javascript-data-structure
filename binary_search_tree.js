3bbccad578e1d79548281746ed923d7901d46c21
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-06-28 16:46:21 +0200

    binary search tree search

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -41,6 +41,27 @@ class BinarySearchTree {
       }
     }
   }
+
+  // return true if found the node
+  // return false if node not found
+  search(value) {
+    if (this.root === null) return false;
+
+    var currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.value === value) return true;
+
+      if (value < currentNode.value) {
+        // to the left
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        //to the right
+        currentNode = currentNode.right;
+      }
+    }
+
+    return false;
+  }
 }
 
 var tree = new BinarySearchTree();
