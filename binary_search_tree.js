5f759e161b4bf0fd6b55f6eeadaedbf0db3922ff
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-07-01 16:20:42 +0200

    binary search tree BFS

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -62,7 +62,37 @@ class BinarySearchTree {
 
     return false;
   }
+
+  //Breadth first search
+  BFS() {
+    var data = [];
+    var queue = [];
+    queue.push(this.root);
+
+    // loop as long as there is anything in the queue
+    var node = null;
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return data;
+  }
 }
 
 var tree = new BinarySearchTree();
 tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.BFS());
