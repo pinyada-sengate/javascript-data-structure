b56d487eb13257ea8a5424bd51ef9b3574bedb03
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-07-11 18:30:18 +0200

    graph remove edge

--- graph.js
+++ graph.js
@@ -14,4 +14,14 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => {
+      v !== vertex2;
+    });
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex1].filter((v) => {
+      v !== vertex1;
+    });
+  }
 }
