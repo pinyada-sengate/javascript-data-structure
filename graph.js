4f1131eb0ada22656d0dbc7db19d2fa33bd126a5
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-07-11 17:52:44 +0200

    graph add vertex

---
+++ graph.js
@@ -0,0 +1,12 @@
+// undirected graph
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+}
