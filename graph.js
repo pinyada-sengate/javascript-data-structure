d72733ec71b5672071022b15362473ba301ad528
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-07-12 10:50:45 +0200

    graph depth first recursive

--- graph.js
+++ graph.js
@@ -24,4 +24,59 @@ class Graph {
       v !== vertex1;
     });
   }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const vertex2 = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, vertex2);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    }
+
+    dfs(start);
+    return result;
+  }
 }
+
+let graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
+console.log(graph.depthFirstRecursive("A"));
+
+//            A
+//          /   \
+//         B     C
+//         |     |
+//         D --- E
+//         \    /
+//            F
