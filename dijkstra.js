7c7ed307b9efddeab35280570341b962deab919b
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-07-15 11:51:18 +0200

    prepare for dijkstra algo

--- dijkstra.js
+++ dijkstra.js
@@ -1,3 +1,24 @@
+// get the smallest thing first
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+
+  // get the smallest priority
+  dequeue() {
+    return this.values.shift();
+  }
+
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
